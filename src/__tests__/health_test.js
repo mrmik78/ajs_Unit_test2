7981b20c3600e6395dea5d0aa56eeb9e8b23b8a9
МС 21 <m.reutsky@mail.ru>
2023-06-21 21:14:22 +0300

    init

---
+++ src/__tests__/health_test.js
@@ -0,0 +1,17 @@
+import sortHealth from '../health_test';
+
+const array = [
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+];
+
+const result = [
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+  { name: 'мечник', health: 10 },
+];
+
+test('sort ascending', () => {
+  expect(sortHealth(array)).toEqual(result);
+});
